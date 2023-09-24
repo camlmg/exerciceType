e4e38c5b7f322bb0f9608482ed56b647a0ba96cb
camlmg <143876043+camlmg@users.noreply.github.com>
2023-09-24 17:57:32 +0200

    Add files via upload

---
+++ exercice.js
@@ -0,0 +1,21 @@
+"use strict"
+let nombreAvengers=8 // nombre
+let nomFamilleJules="César" // chaine de caracteres (string)
+let nombreEtoile= 1000 // nombre (number)
+let bienvenue="bonjour" //chaine de caractere
+let faceID= true //boolean
+let prixOrdinateur= 600 //nombre
+let nomsSeptNains= "Les sept nains sont Prof Simplet..." //string on pourrait faire un tableau, qui serait un type objet
+let bgTinder= "bg78" //string
+
+let cloneNaruto= 7 //number
+let defautsEntertien= "impatient, perfectionniste" //string peut aussi etre un tableau (type objet)
+let prenomFlamme= "Anneeeee" //string
+let panierAmazon= "chargeur"//string
+let guerre= 100 //number
+let biere="pas bon" //string
+let prochainPresident= "chloe" //string
+
+
+
+
